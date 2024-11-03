a7514c5711d94c11d1a80cb292ac69890a6de1b5
Brycerw11 <77603372+Brycerw11@users.noreply.github.com>
2024-11-03 16:09:16 -0700

    count sig figs before .

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int sigFigs = 0;
         Scanner numberScanner = new Scanner(System.in);
 
         String number = numberScanner.nextLine();
@@ -16,12 +17,37 @@ public class Main {
 
         System.out.println(number);
 
+        int dotPos = 0;
+
+        //Find dotPos
         for (int i = 1; i <= number.length(); i++){
             if (i != number.length()){
                 if(Objects.equals(number.substring(i, (i + 1)), ".")){
                     System.out.println("Contains .");
+                    dotPos = i;
                 }
             }
+            else {
+                dotPos = number.length();
+            }
         }
+
+        //Before dot
+        boolean startOfSignificance = false;
+        //Find dotPos
+        for (int i = 1; i < dotPos; i++){
+            if (!startOfSignificance){
+                if (!Objects.equals(number.substring(i, (i + 1)), "0")){
+                    startOfSignificance = true;
+                    sigFigs++;
+                }
+            }
+            else {
+                sigFigs++;
+            }
+        }
+
+        System.out.println(sigFigs);
+
     }
 }
